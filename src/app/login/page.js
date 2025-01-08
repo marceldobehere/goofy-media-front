8fb6d4792ae63c5919670ee8947039217081f59c
Masl <70035395+marceldobehere@users.noreply.github.com>
2025-01-08 10:58:22 +0100

    test with enc

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -4,13 +4,11 @@ export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <h1>Hello</h1>
+        <h1>Login</h1>
       </main>
         <footer className={styles.footer}>
             <p>lol</p>
-
-            <a href={"/login"}>Login</a><br></br>
-            <a href={"/enc"}>Enc</a><br></br>
+            <a href={"/"}>Home</a>
         </footer>
     </div>
   );
