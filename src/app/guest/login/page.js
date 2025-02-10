84f984bf92dc985d1259d1bf73c93bd71690200b
Masl <70035395+marceldobehere@users.noreply.github.com>
2025-02-10 13:34:08 +0100

    wa

--- src/app/guest/login/page.js
+++ src/app/guest/login/page.js
@@ -1,16 +1,19 @@
 'use client';
 
-import styles from "./page.module.css";
+import styles from "@/app/page.module.css";
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
         <h1>Login</h1>
+
+        <div className={"container"}>A</div>
+
       </main>
         <footer className={styles.footer}>
             <p>lol</p>
-            <a href={"/"}>Home</a>
+            <a href={"/user/home"}>Home</a>
         </footer>
     </div>
   );
--- src/app/user/home/page.js
+++ src/app/user/home/page.js
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <h1>Home</h1>
+        <h1>User Home</h1>
       </main>
         <footer className={styles.footer}>
             <p>lol</p>
