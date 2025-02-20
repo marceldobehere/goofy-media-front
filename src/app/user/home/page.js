baebe3c65621c03fb875cc05b5880428cbdede53
Masl <marcel.z2006@gmail.com>
2025-02-20 22:53:12 +0100

    home page and hamburger

--- src/app/user/home/page.js
+++ src/app/user/home/page.js
@@ -2,16 +2,24 @@
 
 import styles from "./page.module.css";
 import {GlobalStuff, initGlobalState} from "@/lib/globalStateStuff";
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 import MainFooter from "@/comp/mainFooter";
 
 export default function Home() {
+    let [showBurgerMenu, setBurger] =  useState(false);
+
     useEffect(() => {
         initGlobalState(async () => {
             if (!GlobalStuff.loggedIn) {
                 window.location.href = "/guest/login";
             }
         });
+
+        window.onresize = () => {
+            if (showBurgerMenu && (window.getComputedStyle(document.documentElement).getPropertyValue('--show_hamburg') === '0')) {
+                setBurger(false);
+            }
+        }
     })
 
 
@@ -19,149 +27,159 @@ export default function Home() {
         <div className={styles.page}>
             <main className={styles.main}>
 
-                <nav className={styles.NavBar}>
-                    <h2>Navigation</h2>
-                    <p>
-                        sad jfhklajsdhfasdfasd<br/>
-                        fadsjfhadsjkhfljkadshfasd<br/>
-                        fasdjfasdjfh ljkdsahfdas<br/>
-                        fadsjhfadskjh fadsf<br/>
-                        asdfjkasdhfkjadsh<br/>
-                        sdjfasljkdfsadkj
-                    </p>
+                <nav className={showBurgerMenu ? styles.NavBar2 : styles.NavBar}>
+                    <div className={styles.NavBarHamburg}>
+                        <button onClick={() => {
+                            setBurger(!showBurgerMenu);
+                        }}>{showBurgerMenu ? "^" : "v"}</button>
+                        <h2>Goofy Media</h2>
+                    </div>
+                    <div className={showBurgerMenu ? styles.None : styles.NavBarDiv}>
+                        {showBurgerMenu ? <></> : <h2>Navigation</h2>}
+                        <p>
+                            <a href={"/"}>Lol 1</a><br/>
+                            <a href={"/"}>Lol 2</a><br/>
+                            <a href={"/"}>Lol 3</a><br/>
+                            <a href={"/"}>Lol 4</a><br/>
+                            <a href={"/"}>Lol 5</a><br/>
+                        </p>
+                    </div>
                 </nav>
 
-                <div className={styles.PostDiv}>
-                    <h1>User Home</h1>
+                <div className={styles.MainContent}>
+                    <div className={styles.PostDiv}>
+                        <h1>User Home</h1>
 
-                    Bla, Bla
+                        Bla, Bla
 
-                    <div>
                         <div>
-                            <h2>Post abc</h2>
-                            <p>
+                            <div>
+                                <h2>Post abc</h2>
+                                <p>
                                 kajsdfh adsf sadg fsadfkjadshgf ads<br/>
-                                a afdsa fhaskdh fadskl flasjdkf lkdjsaf<br/>
-                                sd fads fladskf asjhj fjklsa hjasf<br/>
-                                asdf jasdh fkjadshjklhsdakljf sad fadsf
-                                fasdfasd fasd fasd
-                            </p>
-                        </div>
-                        <div>
-                            <h2>Post abc</h2>
-                            <p>
-                                kajsdfh adsf sadg fsadfkjadshgf ads<br/>
-                                a afdsa fhaskdh fadskl flasjdkf lkdjsaf<br/>
-                                sd fads fladskf asjhj fjklsa hjasf<br/>
-                                asdf jasdh fkjadshjklhsdakljf sad fadsf
-                                fasdfasd fasd fasd
-                            </p>
-                        </div>
-                        <div>
-                            <h2>Post abc</h2>
-                            <p>
-                                kajsdfh adsf sadg fsadfkjadshgf ads<br/>
-                                a afdsa fhaskdh fadskl flasjdkf lkdjsaf<br/>
-                                sd fads fladskf asjhj fjklsa hjasf<br/>
-                                asdf jasdh fkjadshjklhsdakljf sad fadsf<br/>
-                                fasdfasd fasd fasd
-                            </p>
-                        </div>
-                        <div>
-                            <h2>Post abc</h2>
-                            <p>
-                                kajsdfh adsf sadg fsadfkjadshgf ads
-                                a afdsa fhaskdh fadskl flasjdkf lkdjsaf
-                                sd fads fladskf asjhj fjklsa hjasf<br/>
-                                asdf jasdh fkjadshjklhsdakljf sad fadsf<br/>
-                                fasdfasd fasd fasd<br/>
-                            </p>
-                        </div>
-                        <div>
-                            <h2>Post abc</h2>
-                            <p>
-                                kajsdfh adsf sadg fsadfkjadshgf ads<br/>
-                                a afdsa fhaskdh fadskl flasjdkf lkdjsaf<br/>
-                                sd fads fladskf asjhj fjklsa hjasf<br/>
-                                asdf jasdh fkjadshjklhsdakljf sad fadsf
-                                fasdfasd fasd fasd
-                            </p>
-                        </div>
+                                    a afdsa fhaskdh fadskl flasjdkf lkdjsaf<br/>
+                                    sd fads fladskf asjhj fjklsa hjasf<br/>
+                                    asdf jasdh fkjadshjklhsdakljf sad fadsf
+                                    fasdfasd fasd fasd
+                                </p>
+                            </div>
+                            <div>
+                                <h2>Post abc</h2>
+                                <p>
+                                    kajsdfh adsf sadg fsadfkjadshgf ads<br/>
+                                    a afdsa fhaskdh fadskl flasjdkf lkdjsaf<br/>
+                                    sd fads fladskf asjhj fjklsa hjasf<br/>
+                                    asdf jasdh fkjadshjklhsdakljf sad fadsf
+                                    fasdfasd fasd fasd
+                                </p>
+                            </div>
+                            <div>
+                                <h2>Post abc</h2>
+                                <p>
+                                    kajsdfh adsf sadg fsadfkjadshgf ads<br/>
+                                    a afdsa fhaskdh fadskl flasjdkf lkdjsaf<br/>
+                                    sd fads fladskf asjhj fjklsa hjasf<br/>
+                                    asdf jasdh fkjadshjklhsdakljf sad fadsf<br/>
+                                    fasdfasd fasd fasd
+                                </p>
+                            </div>
+                            <div>
+                                <h2>Post abc</h2>
+                                <p>
+                                    kajsdfh adsf sadg fsadfkjadshgf ads
+                                    a afdsa fhaskdh fadskl flasjdkf lkdjsaf
+                                    sd fads fladskf asjhj fjklsa hjasf<br/>
+                                    asdf jasdh fkjadshjklhsdakljf sad fadsf<br/>
+                                    fasdfasd fasd fasd<br/>
+                                </p>
+                            </div>
+                            <div>
+                                <h2>Post abc</h2>
+                                <p>
+                                    kajsdfh adsf sadg fsadfkjadshgf ads<br/>
+                                    a afdsa fhaskdh fadskl flasjdkf lkdjsaf<br/>
+                                    sd fads fladskf asjhj fjklsa hjasf<br/>
+                                    asdf jasdh fkjadshjklhsdakljf sad fadsf
+                                    fasdfasd fasd fasd
+                                </p>
+                            </div>
 
 
+                        </div>
+                    </div>
+
+                    <div className={styles.NewsDiv}>
+                        <h2>Goofy Media News</h2>
+
+                        <p>
+                            asdf sadf<br/>
+                            asdf asdf asdf<br/>
+                            asdf asd fasdf asdf<br/>
+                            sdaf adsf asdf sadf sad<br/>
+                            sdaf adsf asdf sadf sad<br/>
+                            sdaf adsf asdf sadf sad<br/>
+                            sdaf adsf asdf sadf sad<br/>
+                            sdaf adsf asdf sadf sad<br/>
+                            sdaf adsf asdf sadf sad<br/>
+                            sdaf adsf asdf sadf sad<br/>
+                            sdaf adsf asdf sadf sad<br/>
+                            sdaf adsf asdf sadf sad<br/>
+                            sdaf adsf asdf sadf sad<br/>
+                            sdaf adsf asdf sadf sad<br/>
+                            sdaf adsf asdf sadf sad<br/>
+                            sdaf adsf asdf sadf sad<br/>
+                            sdaf adsf asdf sadf sad<br/>
+                            sdaf adsf asdf sadf sad<br/>
+                            sdaf adsf asdf sadf sad<br/>
+                            sdaf adsf asdf sadf sad<br/>
+                            sdaf adsf asdf sadf sad<br/>
+                            sdaf adsf asdf sadf sad<br/>
+                            sdaf adsf asdf sadf sad<br/>
+                            sdaf adsf asdf sadf sad<br/>
+                            sdaf adsf asdf sadf sad<br/>
+                            sdaf adsf asdf sadf sad<br/>
+                            sdaf adsf asdf sadf sad<br/>
+                            sdaf adsf asdf sadf sad<br/>
+                            sdaf adsf asdf sadf sad<br/>
+                            sdaf adsf asdf sadf sad<br/>
+                            sdaf adsf asdf sadf sad<br/>
+                            sdaf adsf asdf sadf sad<br/>
+                            sdaf adsf asdf sadf sad<br/>
+                            sdaf adsf asdf sadf sad<br/>
+                            sdaf adsf asdf sadf sad<br/>
+                            sdaf adsf asdf sadf sad<br/>
+                            sdaf adsf asdf sadf sad<br/>
+                            sdaf adsf asdf sadf sad<br/>
+                            sdaf adsf asdf sadf sad<br/>
+                            sdaf adsf asdf sadf sad<br/>
+                            sdaf adsf asdf sadf sad<br/>
+                            sdaf adsf asdf sadf sad<br/>
+                            sdaf adsf asdf sadf sad<br/>
+                            sdaf adsf asdf sadf sad<br/>
+                            sdaf adsf asdf sadf sad<br/>
+                            sdaf adsf asdf sadf sad<br/>
+                            sdaf adsf asdf sadf sad<br/>
+                            sdaf adsf asdf sadf sad<br/>
+                            sdaf adsf asdf sadf sad<br/>
+                            sdaf adsf asdf sadf sad<br/>
+                            sdaf adsf asdf sadf sad<br/>
+                            sdaf adsf asdf sadf sad<br/>
+                            sdaf adsf asdf sadf sad<br/>
+                            sdaf adsf asdf sadf sad<br/>
+                            sdaf adsf asdf sadf sad<br/>
+                            sdaf adsf asdf sadf sad<br/>
+                            sdaf adsf asdf sadf sad<br/>
+                            sdaf adsf asdf sadf sad<br/>
+                            sdaf adsf asdf sadf sad<br/>
+                            sdaf adsf asdf sadf sad<br/>
+                            sdaf adsf asdf sadf sad<br/>
+                            sdaf adsf asdf sadf sad<br/>
+                            sdaf adsf asdf sadf sad<br/>
+                            f asdf saads
+                        </p>
                     </div>
                 </div>
 
-                <div className={styles.NewsDiv}>
-                    <h2>Goofy Media News</h2>
-
-                    <p>
-                        asdf sadf<br/>
-                        asdf asdf asdf<br/>
-                        asdf asd fasdf asdf<br/>
-                        sdaf adsf asdf sadf sad<br/>
-                        sdaf adsf asdf sadf sad<br/>
-                        sdaf adsf asdf sadf sad<br/>
-                        sdaf adsf asdf sadf sad<br/>
-                        sdaf adsf asdf sadf sad<br/>
-                        sdaf adsf asdf sadf sad<br/>
-                        sdaf adsf asdf sadf sad<br/>
-                        sdaf adsf asdf sadf sad<br/>
-                        sdaf adsf asdf sadf sad<br/>
-                        sdaf adsf asdf sadf sad<br/>
-                        sdaf adsf asdf sadf sad<br/>
-                        sdaf adsf asdf sadf sad<br/>
-                        sdaf adsf asdf sadf sad<br/>
-                        sdaf adsf asdf sadf sad<br/>
-                        sdaf adsf asdf sadf sad<br/>
-                        sdaf adsf asdf sadf sad<br/>
-                        sdaf adsf asdf sadf sad<br/>
-                        sdaf adsf asdf sadf sad<br/>
-                        sdaf adsf asdf sadf sad<br/>
-                        sdaf adsf asdf sadf sad<br/>
-                        sdaf adsf asdf sadf sad<br/>
-                        sdaf adsf asdf sadf sad<br/>
-                        sdaf adsf asdf sadf sad<br/>
-                        sdaf adsf asdf sadf sad<br/>
-                        sdaf adsf asdf sadf sad<br/>
-                        sdaf adsf asdf sadf sad<br/>
-                        sdaf adsf asdf sadf sad<br/>
-                        sdaf adsf asdf sadf sad<br/>
-                        sdaf adsf asdf sadf sad<br/>
-                        sdaf adsf asdf sadf sad<br/>
-                        sdaf adsf asdf sadf sad<br/>
-                        sdaf adsf asdf sadf sad<br/>
-                        sdaf adsf asdf sadf sad<br/>
-                        sdaf adsf asdf sadf sad<br/>
-                        sdaf adsf asdf sadf sad<br/>
-                        sdaf adsf asdf sadf sad<br/>
-                        sdaf adsf asdf sadf sad<br/>
-                        sdaf adsf asdf sadf sad<br/>
-                        sdaf adsf asdf sadf sad<br/>
-                        sdaf adsf asdf sadf sad<br/>
-                        sdaf adsf asdf sadf sad<br/>
-                        sdaf adsf asdf sadf sad<br/>
-                        sdaf adsf asdf sadf sad<br/>
-                        sdaf adsf asdf sadf sad<br/>
-                        sdaf adsf asdf sadf sad<br/>
-                        sdaf adsf asdf sadf sad<br/>
-                        sdaf adsf asdf sadf sad<br/>
-                        sdaf adsf asdf sadf sad<br/>
-                        sdaf adsf asdf sadf sad<br/>
-                        sdaf adsf asdf sadf sad<br/>
-                        sdaf adsf asdf sadf sad<br/>
-                        sdaf adsf asdf sadf sad<br/>
-                        sdaf adsf asdf sadf sad<br/>
-                        sdaf adsf asdf sadf sad<br/>
-                        sdaf adsf asdf sadf sad<br/>
-                        sdaf adsf asdf sadf sad<br/>
-                        sdaf adsf asdf sadf sad<br/>
-                        sdaf adsf asdf sadf sad<br/>
-                        sdaf adsf asdf sadf sad<br/>
-                        sdaf adsf asdf sadf sad<br/>
-                        f asdf saads
-                    </p>
-                </div>
             </main>
             <MainFooter></MainFooter>
         </div>
