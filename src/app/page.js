e906bc6fd0c83cc9e5c5f0fcbc46df4d25ee9829
Masl <marcel.z2006@gmail.com>
2025-02-23 02:07:54 +0100

    yes

--- src/app/page.js
+++ src/app/page.js
@@ -8,6 +8,13 @@ export default function Home() {
             <main className={styles.main}>
                 <h1>Goofy Media</h1>
 
+                <p>
+                    Work In Progress Goofy Social Media app<br/>
+                    Open source <a href={"https://github.com/marceldobehere/goofy-media-front"}>here</a><br/>
+                    Made by <a href={"https://github.com/marceldobehere"}>@marceldobehere</a>.
+                </p>
+                <br/>
+
                 <Link href={"/guest/login"}>Login</Link><br/>
                 <Link href={"/guest/register"}>Register</Link><br/>
                 <Link href={"/user/home"}>Home</Link><br/>
