270c7eb02c7b122e7bc19b4b012d72221da168b3
Masl <marcel.z2006@gmail.com>
2025-02-24 23:00:55 +0100

    lol2

--- src/app/user/home/page.js
+++ src/app/user/home/page.js
@@ -138,7 +138,7 @@ export default function Home() {
         prevPos = pos;
 
         // check if button is visible on screen
-        if (pos < window.innerHeight + 300 && pos > 0) {
+        if (pos < window.innerHeight + 300 && pos > window.innerHeight - 180) {
             loadMorePosts();
         }
     }
