d77ef182e0ced37bdeb669381cac3a6ad99389fb
Masl <70035395+marceldobehere@users.noreply.github.com>
2025-03-18 11:15:46 +0100

    fixed the tag links not working in the search

--- src/app/user/home/entries/postEntry.js
+++ src/app/user/home/entries/postEntry.js
@@ -1,7 +1,8 @@
 import styles from "@/app/user/home/entries/postEntry.module.css";
-import Link from "next/link";
+import {goPath} from "@/lib/goPath";
 
 export default function PostEntry({post}) {
+
     return (
         <div className={styles.PostEntryDiv}>
             <div className={styles.PostUserHeader}>
@@ -10,7 +11,9 @@ export default function PostEntry({post}) {
 
             <h3 className={styles.PostEntryHeader}>{post.title}</h3>
             <p className={styles.PostBody}>{post.text}</p>
-            <p>Tags: {post.tags.map((tag, idx) => (<span key={idx}><Link href={`/guest/search?tag=${encodeURIComponent(tag)}`}>#{tag}</Link>&nbsp;</span>))}</p>
+            <p>Tags: {post.tags.map((tag, idx) => (<span key={idx}><a onClick={() => {
+                goPath(`/guest/search?tag=${encodeURIComponent(tag)}`)
+            }}>#{tag}</a>&nbsp;</span>))}</p>
         </div>
     );
 }
